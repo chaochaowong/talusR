2e0517f72fbae2cada7e4484ad7f05100a3ff186
Chao-Jen Wong <chao-jen.wong@seattlechildrens.org>
2025-05-06 13:16:27 -0700

    delete junk lines in summay_talus()

--- R/summaries.R
+++ R/summaries.R
@@ -7,6 +7,7 @@
 #'
 #' @importFrom purrr map
 #' @importFrom dplyr filter
+#'
 #' @export
 summary_talus <- function(object,
                           alpha = NULL,
@@ -22,7 +23,6 @@ summary_talus <- function(object,
   #if (is.list(object)) {
     # object should be a list
     # get the filtered data.frame
-    obj_filt <- map(object, summary_per_contrast, alpha, lfc_threshold)
     # display message
     obj_filt <- map2(object, names(object), display_summary_talus,
                      alpha, lfc_threshold)
@@ -41,6 +41,9 @@ display_summary_talus <- function(res,
                             alpha,
                             lfc_threshold) {
 
+  # must check if the input is a data.frame containing either adj.P.Val or logFC columns
+  #if (!is.data.frame(res))
+  #  stop('')
   filt <- res
   if (!is.null(alpha))
     filt <- filt %>%
@@ -74,40 +77,3 @@ display_summary_talus <- function(res,
   return(filt)
 }
 
-display_summary_twalch <- function(res,
-                                  contrast_name,
-                                  alpha,
-                                  lfc_threshold) {
-
-  filt <- res
-  if (!is.null(alpha))
-    filt <- filt %>%
-      dplyr::filter(adj.P.Val< alpha)
-
-  if (!is.null(lfc_threshold))
-    filt <- filt %>%
-      dplyr::filter(abs(logFC) > abs(lfc_threshold))
-
-  total_features <- nrow(res)
-  n_up           <- sum(filt$logFC >  0, na.rm = TRUE)
-  n_down         <- sum(filt$logFC <  0, na.rm = TRUE)
-  n_zero         <- sum(filt$logFC == 0, na.rm = TRUE)
-
-  # print a summary
-  cat(contrast_name, ':\n\n')
-  cat(sprintf('Threshold: alpha < %s; |lfc_threshod| > %s\n',
-              if (is.null(alpha)) "NULL" else alpha,
-              if (is.null(lfc_threshold)) "NULL" else lfc_threshold))
-  cat(
-    sprintf(
-      "Total features: %d\nUp-regulated:   %d\n",
-      total_features, n_up
-    ),
-    sprintf("Down-regulated: %d\n", n_down),
-    if (n_zero > 0) sprintf("No change:   %d\n", n_zero) else "",
-    sep = ""
-  )
-
-  cat('\n')
-  return(filt)
-}
