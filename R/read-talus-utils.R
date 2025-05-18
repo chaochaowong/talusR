ac2892919ef20dbcea65067c2d1153a62e87bc8e
Chao-Jen Wong <chao-jen.wong@seattlechildrens.org>
2025-05-18 13:39:01 -0700

    add content to vignette

--- R/read-talus-utils.R
+++ R/read-talus-utils.R
@@ -17,14 +17,15 @@
       split(.[[which_fraction]]) %>%       # named list by Fraction values
       map(function(sub_meta) {
         .construct_tds(sub_meta,
-                      which_run,
-                      tb,
-                      which_proteinid,
-                      all_runs,
-                      intensity_group,
-                      metric,
-                      log_transform)
+                       which_run,
+                       tb,
+                       which_proteinid,
+                       all_runs,
+                       intensity_group,
+                       metric,
+                       log_transform)
       })
+
     tds <- TalusDataSetList(tds)
     # TODO? clean up rownames: one single protein.ID ??
 
@@ -46,17 +47,15 @@
   return(tds)
 }
 
-
-
 .construct_tds <- function(sub_meta,
-                          which_run,
-                          tb,
-                          which_proteinid,
-                          all_runs,
-                          intensity_group = "protein",
-                          metric = "DIA-NN",
-                          log_transform = 'log2') {
-  offset = 1
+                           which_run,
+                           tb,
+                           which_proteinid,
+                           all_runs,
+                           intensity_group = "protein",
+                           metric = "DIA-NN",
+                           log_transform = 'log2') {
+  #offset = 1
   runs <- sub_meta[[which_run]]
   # assay
   assay <- tb %>% dplyr::select(any_of(runs)) %>%
@@ -72,17 +71,12 @@
   row_data <- as(row_data, 'DataFrame')
   rownames(row_data) <- tb %>% pull(which_proteinid)
 
-  se <- TalusDataSet(assay_data = assay,
-                     col_data = col_data,
-                     row_data = row_data,
-                     intensity_group = intensity_group,
-                     metric = metric)
-
-  #se <- SummarizedExperiment(assays=log2(assay+offset),
-  #                           rowData=row_data,
-  #                           colData=col_data,
-  #                           metadata=col_data,
-  #                           checkDimnames=TRUE)
+  TalusDataSet(assay_data = assay,
+               col_data = col_data,
+               row_data = row_data,
+               intensity_group = intensity_group,
+               metric = metric,
+               log_transform = log_transform)
 }
 
 .remove_few_measurements <- function(x, threshold = 0.85) {
